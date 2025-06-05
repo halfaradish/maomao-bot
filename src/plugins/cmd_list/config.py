61906bc04d09d67e8349df0f734da54636e441fd
half <half85405@gmail.com>
2025-06-05 23:03:57 +0800

    feat: 列出命令列表初版

--- src/plugins/cmd_list/config.py
+++ src/plugins/cmd_list/config.py
@@ -3,3 +3,14 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     """Plugin Config Here"""
+    # 响应优先级
+    priority = 10
+    # 是否阻塞
+    block = True
+
+    user_msg = """/cmd 列出所有命令
+    /赞我 点赞10次"""
+
+    editor_msg = "/考勤 查看考勤情况(yyyy-mm-dd range)"
+
+    admin_msg = ''
