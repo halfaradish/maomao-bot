c0f20c0e2ebac820a9ad34a87763e4a60d2e4144
half <half85405@gmail.com>
2025-06-08 02:10:31 +0800

    style: 修改考勤配置

--- src/plugins/check_up/config.py
+++ src/plugins/check_up/config.py
@@ -4,6 +4,11 @@ from typing import List
 
 class Config(BaseModel):
     """Plugin Config Here"""
+    # 优先级
+    priority: int = 10
+    # 是否阻塞
+    block: bool = True
+
     # 要发送定时消息的群
     GROUP_IDS: List[int] = [779245720]
     # 定时发送的时间
@@ -11,7 +16,14 @@ class Config(BaseModel):
     TIMING_MINUTE: str = '00'
     TIMING_SECOND: str = '00'
 
-    # 优先级
-    priority: int = 10
-    # 是否阻塞
-    block: bool = True
+
+    # /考勤 默认提示词
+    DEFAULT_MSG: str = (
+        "[考勤]命令使用方法\n"
+        "参数:\n"
+        "[日期(date)]: 起始日期, 格式(yyyy-mm-dd)\n"
+        "[范围(range)]: 展示从起始日期开始的之前x天的考勤范围\n"
+        "示例:\n"
+        "\"考勤 7\"-展示最近7天的考勤数据\n"
+        "\"考勤 2025-6-8 3\"-展示从指定日期开始前3天的考勤数据"
+    )
