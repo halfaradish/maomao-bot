7fe1329a6f3cffabb56d22d3a3476719d047c010
half <half85405@gmail.com>
2025-06-11 11:14:06 +0800

    feat: 实现推题模块的rating和tags推题

--- src/plugins/duel/duel.py
+++ src/plugins/duel/duel.py
@@ -3,6 +3,7 @@ from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import MessageEvent
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
+import re
 
 from .config import Config
 from .get_problem import get_one_problem_by_random, get_problem_id_by_rating_tags
@@ -30,11 +31,21 @@ async def vaildate_rating(bot: Bot, event: MessageEvent, rating_str: str):
         return None
     return int(rating_str)
 
+def text_msg_to_params(raw_args: str):
+    parts = re.findall(r"(?<!\\)'(.*?)(?<!\\)'|(?<!\\)\"(.*?)(?<!\\)\"|(\S+)", raw_args)
+    params = []
+    for part in parts:
+        for item in part:
+            if item:
+                params.append(item)
+    return params
+
 @duel_command.handle()
 async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     try:
         raw_args = args.extract_plain_text().strip()
-        params = raw_args.split() if raw_args else []
+        # params = raw_args.split() if raw_args else []
+        params = text_msg_to_params(raw_args=raw_args)
 
         if not params:
             await bot.send(event=event, message=plugin_config.DEFAULT_MSG)
@@ -46,9 +57,13 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
         elif params[0] == 'problem':
             rating = await vaildate_rating(bot=bot, event=event, rating_str=params[1])
             tags = []
-            for param in params[2:]:
-                tags.append(param)
+            if len(params) > 2:
+                for param in params[2:]:
+                    tags.append(param)
             await bot.send(event=event, message=get_problem_id_by_rating_tags(rating=rating, tags=tags))
-
+            return
+        else:
+            await bot.send(event=event, message=f"位置命令: {params[0]}")
+            return
     except Exception as e:
         logger.opt(exception=True).error("[duel]响应错误")
