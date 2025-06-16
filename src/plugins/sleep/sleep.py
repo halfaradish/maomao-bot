30012a255176c84996a40bf6c278f012bc11a4d8
half <half85405@gmail.com>
2025-06-16 19:45:42 +0800

    feat: 延迟处理只处理消息事件

--- src/plugins/sleep/sleep.py
+++ src/plugins/sleep/sleep.py
@@ -3,6 +3,7 @@ import random
 from nonebot import get_driver, get_plugin_config, logger
 from nonebot.adapters import Event
 from nonebot.plugin import PluginMetadata
+from nonebot.adapters.onebot.v11 import MessageEvent
 from nonebot.message import event_preprocessor
 
 from .config import Config
@@ -24,6 +25,13 @@ async def global_random_delay(event: Event):
     if not delay_enabled:
         return
     
-    delay = random.uniform(plugin_config.min_sleep_time, plugin_config.max_sleep_time)
+    # 仅处理MessageEvent事件
+    if not isinstance(event, MessageEvent):
+        return
+    
+    min_time = plugin_config.min_sleep_time
+    max_time = plugin_config.max_sleep_time
+    
+    delay = random.uniform(min_time, max_time)
     logger.opt(exception=True).info(f"随机延迟{delay:.2f}秒回复")
     await asyncio.sleep(delay)
