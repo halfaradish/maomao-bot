b8ef8faa1f31899e5b5615117a3d5d3c3a734481
half <half85405@gmail.com>
2025-08-03 16:59:20 +0800

    refector: 重构推题功能

--- src/plugins/duel/duel.py
+++ src/plugins/duel/duel.py
@@ -1,4 +1,4 @@
-from nonebot import Bot, on_command, logger, get_driver, get_plugin_config
+from nonebot import Bot, on_command, logger, get_plugin_config
 from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import MessageEvent
 from nonebot.adapters import Message
@@ -25,182 +25,185 @@ duel_command = on_command(
     block=plugin_config.block
 )
 
-def regex_search(str_list: list[str], pattern: str) -> list[str]:
-    """对列表进行模糊搜索"""
-    result = []
-    for str in str_list:
-        if re.search(pattern, str, re.IGNORECASE):
-            result.append(str)
-    return result
+class CommandHandler:
+    @staticmethod
+    def regex_search(str_list: list[str], pattern: str) -> list[str]:
+        """对列表进行模糊搜索"""
+        return [s for s in str_list if re.search(pattern, s, re.IGNORECASE)]
 
-def rating_or_tag(bot: Bot, event: MessageEvent, confirmed_str: str):
-    """判断是rating还是tag"""
-    if not confirmed_str.isdigit():
-        return confirmed_str
-    return int(confirmed_str)
+    @staticmethod
+    def text_msg_to_params(raw_args: str) -> list[str]:
+        """解析带引号的参数"""
+        parts = re.findall(r"(?:^|\s)(?:'(.*?)'|\"(.*?)\"|(\S+))", raw_args)
+        return [item for part in parts for item in part if item]
 
-def text_msg_to_params(raw_args: str):
-    # parts = re.findall(r"(?<!\\)'(.*?)(?<!\\)'|(?<!\\)\"(.*?)(?<!\\)\"|(\S+)", raw_args)
-    parts = re.findall(r"(?:^|\s)(?:'(.*?)'|\"(.*?)\"|(\S+))", raw_args)
-    params = []
-    for part in parts:
-        for item in part:
-            if item:
-                params.append(item)
-    return params
+    @staticmethod
+    def handle_daily(bot: Bot, event: MessageEvent):
+        """处理每日一题命令"""
+        bot.send(event=event, message=get_one_problem_by_random())
+
+    @staticmethod
+    def handle_problem(bot: Bot, event: MessageEvent, params: list[str]):
+        """处理题目查询命令"""
+        if len(params) < 1:
+            bot.send(event=event, message=f"该命令可在 problem 后可添加一个 rating 参数和多个 tag 参数。如\"duel problem 2300 dp 'binary search'，并根据输入的参数推题\"")
+            return
+
+        rating = None
+        rating_cnt = 0
+        tags = []
+        for param in params:
+            if param.isdigit():
+                rating_cnt += 1
+                if rating_cnt > 1:
+                    bot.send(event=event, message=f"该命令只接收一个 rating 参数，请检查参数{params}")
+                    return
+                rating = int(param)
+            else:
+                tags.append(param)
+        
+        if tags:
+            data, _ = JsonUtils.read(plugin_config.filename, {})
+            tags_quick_map = data.get("quick_map", {})
+            tags = [tags_quick_map.get(tag, tag) for tag in tags]
+        
+        bot.send(event=event, message=get_problem_id_by_rating_tags(rating, tags))
+
+    @staticmethod
+    def handle_map_tags(data: dict) -> str:
+        """列出所有可用标签"""
+        tags_map = data.get("map", {})
+        return "\n".join(tags_map.keys())
+
+    @staticmethod
+    def handle_map_current(data: dict) -> str:
+        """列出当前映射关系"""
+        tags_map = data.get("map", {})
+        if not tags_map:
+            return "当前没有任何标签映射，请使用\n/duel map add\n命令添加新的映射。"
+        
+        lines = []
+        for key, values in tags_map.items():
+            if values:
+                sub_msg = "".join(f"[{value}]" for value in values)
+                lines.append(f"{key} -> {sub_msg}")
+        return "\n".join(lines)
+
+    @staticmethod
+    def handle_map_add(bot: Bot, event: MessageEvent, params: list[str], data: dict):
+        """添加映射关系"""
+        if len(params) != 2:
+            bot.send(event=event, message=f"参数个数有误！需要两个参数，实际收到{len(params)}个参数")
+            return
+        
+        map_key, map_value = params
+        tags_map = data.get("map", {})
+        tags_quick_map = data.get("quick_map", {})
+        
+        if map_key not in tags_map:
+            may_mention_key = CommandHandler.regex_search(list(tags_map.keys()), map_key)
+            if not may_mention_key:
+                msg = f"输入的参数 '{map_key}' 不在标准的 tags 中，请输入\n/duel map tags\n查看可映射的 tags"
+            else:
+                msg = f"输入的参数 '{map_key}' 不在标准的 tags 中\n是否在找{may_mention_key}?\n如果不是，请输入\n/duel map tags\n查看可映射的 tags"
+            bot.send(event=event, message=msg)
+            return
+        
+        if map_value in tags_map[map_key]:
+            bot.send(event=event, message=f"当前映射已存在 '{map_key}' - '{map_value}'")
+            return
+        
+        tags_map[map_key].append(map_value)
+        tags_quick_map[map_value] = map_key
+        JsonUtils.update(plugin_config.filename, {
+            "map": tags_map,
+            "quick_map": tags_quick_map
+        })
+        bot.send(event=event, message=f"映射键值对添加成功：'{map_key}' - '{map_value}'\n可通过\n/duel map current\n查看")
+
+    @staticmethod
+    def handle_map_remove(bot: Bot, event: MessageEvent, params: list[str], data: dict):
+        """删除映射关系"""
+        if len(params) != 2:
+            bot.send(event=event, message=f"参数个数有误！需要两个参数，实际收到{len(params)}个参数")
+            return
+        
+        map_key, map_value = params
+        tags_map = data.get("map", {})
+        tags_quick_map = data.get("quick_map", {})
+        
+        if map_key not in tags_map:
+            may_mention_key = CommandHandler.regex_search(list(tags_map.keys()), map_key)
+            if not may_mention_key:
+                msg = f"输入的参数 '{map_key}' 不在标准的 tags 中，请输入\n/duel map tags\n查看可映射的 tags"
+            else:
+                msg = f"输入的参数 '{map_key}' 不在标准的 tags 中\n是否在找{may_mention_key}?\n如果不是，请输入\n/duel map tags\n查看可映射的 tags"
+            bot.send(event=event, message=msg)
+            return
+        
+        if map_value not in tags_map[map_key]:
+            bot.send(event=event, message=f"当前映射不存在 '{map_key}' : '{map_value}'")
+            return
+        
+        tags_map[map_key].remove(map_value)
+        tags_quick_map.pop(map_value)
+        JsonUtils.update(plugin_config.filename, {
+            "map": tags_map,
+            "quick_map": tags_quick_map
+        })
+        bot.send(event=event, message=f"映射键值对删除成功：'{map_key}' : '{map_value}'")
+
+    @staticmethod
+    def handle_map(bot: Bot, event: MessageEvent, params: list[str]):
+        """处理映射命令"""
+        data, _ = JsonUtils.read(plugin_config.filename, {})
+        
+        if not params:
+            bot.send(event=event, message=plugin_config.MAP_DEFAULT_MSG)
+            return
+        
+        subcommand = params[0]
+        sub_params = params[1:]
+        
+        handlers = {
+            "tags": lambda: bot.send(event=event, message=CommandHandler.handle_map_tags(data)),
+            "current": lambda: bot.send(event=event, message=CommandHandler.handle_map_current(data)),
+            "add": lambda: CommandHandler.handle_map_add(bot, event, sub_params, data),
+            "rm": lambda: CommandHandler.handle_map_remove(bot, event, sub_params, data),
+            "remove": lambda: CommandHandler.handle_map_remove(bot, event, sub_params, data)
+        }
+        
+        if subcommand in handlers:
+            handlers[subcommand]()
+        else:
+            bot.send(event=event, message=f"map 后跟了未知参数: {subcommand}，请使用\n/duel map\n查看可用的命令")
 
 @duel_command.handle()
-async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+async def handle_duel_command(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     try:
         raw_args = args.extract_plain_text().strip()
-        # params = raw_args.split() if raw_args else []
-        params = text_msg_to_params(raw_args=raw_args)
-
+        params = CommandHandler.text_msg_to_params(raw_args) if raw_args else []
+        
         if not params:
             await bot.send(event=event, message=plugin_config.DEFAULT_MSG)
             return
-
-        if params[0] in ['daily', 'day']: # 第一个参数为 daily 时
-            await bot.send(event=event, message=get_one_problem_by_random())
-            return
-        elif params[0] in ['problem', '题目']: # 第一个参数为 problem 时
-            problem_opt_params: str = params[1:]
-            if not problem_opt_params:
-                await bot.send(event=event, message=f"该命令可在 problem 后可添加一个 rating 参数和多个 tag 参数。如\"duel problem 2300 dp 'binary search'，并根据输入的参数推题\"")
-                return
-            rating = None
-            rating_cnt: int = 0
-            tags: list[str] = []
-            for opt_param in problem_opt_params:
-                if opt_param.isdigit():
-                    rating_cnt += 1
-                    if rating_cnt > 1:
-                        await bot.send(event=event, message=f"该命令只接收一个 rating 参数，请检查参数{problem_opt_params}")
-                        return
-                    rating = int(opt_param)
-                else:
-                    tags.append(opt_param)
-            if tags:
-                data, _ = JsonUtils.read(plugin_config.filename, {})
-                tags_quick_map: dict[str, list[str]] = data.get("quick_map", {})
-                for i in range(len(tags)):
-                    if tags[i] in tags_quick_map:
-                        tags[i] = tags_quick_map.get(tags[i], "")
-            res_msg = get_problem_id_by_rating_tags(rating=rating, tags=tags)
-            #region
-            # rating = rating_or_tag(bot=bot, event=event, confirmed_str=params[1])
-            # tags: list[str] = []
-            # if len(params) > 2:
-            #     for param in params[2:]:
-            #         tags.append(param)
-            #     data, _ = JsonUtils.read(plugin_config.filename, {})
-            #     tags_quick_map: dict[str, list[str]] = data.get("quick_map", {})
-            #     for i in range(len(tags)):
-            #         if tags[i] in tags_quick_map:  # 如果tags参数存在映射
-            #             tags[i] = tags_quick_map.get(tags[i], "")
-            #endregion
-            await bot.send(event=event, message=res_msg)
-            return
-        elif params[0] in ['map', '映射']: # 第一个参数为 map 时
-            """映射相关操作"""
-            map_opt_params = params[1:]
-            # 没有 第二个参数 时, 时发送默认消息
-            if not map_opt_params:
-                await bot.send(event=event, message=plugin_config.MAP_DEFAULT_MSG)
-            else:
-                data, _ = JsonUtils.read(plugin_config.filename, {}) # 提取json中的数据
-                tags_map: dict[str, list[str]] = data.get("map", {})
-                tags_quick_map: dict[str, str] = data.get("quick_map", {})
-                for key in tags_map:
-                    if not isinstance(tags_map[key], list):
-                        tags_map[key] = []
-                res_msg = ""
-                # 当map后面的参数为tags时, 列出可添加映射的标签
-                if map_opt_params[0] in ['tags', '标签']:
-                    for key in tags_map:
-                        res_msg += f"{key}\n"
-                    await bot.send(event=event, message=res_msg.strip())
-                    return
-                # 当map后面的参数为current时, 列出已经添加的映射
-                elif map_opt_params[0] in ['current', '现存', '现存标签']:
-                    for key in tags_map:  # 遍历 map 中的所有键
-                        sub_msg = ""
-                        values: list[str] = tags_map.get(key, "")
-                        if values:
-                            for value in values:
-                                sub_msg += f"[{value}]"
-                            res_msg += f"{key} -> {sub_msg}\n"
-                    if res_msg == "":
-                        res_msg = "当前没有任何标签映射，请使用\n/duel map add\n命令添加新的映射。"
-                    await bot.send(event=event, message=res_msg.strip())
-                    return
-                # 当map后面的参数为add时，实行添加映射的操作
-                elif map_opt_params[0] in ['add', '添加']:
-                    if len(map_opt_params) == 1:
-                        await bot.send(event=eval, message=f"该命令在 add 后接收两个参数，第一个参数是存在的可映射的 tag ，第二个参数是 tag 的映射。如\"duel map add 'binary search' 二分\"")
-                        return
-                    elif len(map_opt_params) != 3:
-                        await bot.send(event=event, message=f"参数个数有误！！！\n该命令在 add 后面只接收两个参数，请检查当前参数：{map_opt_params}\n可以使用命令\n/duel map add\n查看命令使用详细")
-                        return
-                    map_key = map_opt_params[1]
-                    map_value = map_opt_params[2]
-                    # 如果输入的要映射的 tag 不在标准的 tags 中
-                    if map_key not in tags_map:
-                        may_mention_key: list[str] = regex_search(list(tags_map.keys()), map_key)
-                        if not may_mention_key:
-                            await bot.send(event=event, message=f"输入的参数 '{map_key}' 不在标准的 tags 中，请输入\n/duel map tags\n查看可映射的 tags")
-                            return
-                        else:
-                            await bot.send(event=event, message=f"输入的参数 '{map_key}' 不在标准的 tags 中\n是否在找{may_mention_key}?\n如果不是，请输入\n/duel map tags\n查看可映射的 tags")
-                            return
-                    else:
-                        if map_value in tags_map[map_key]:
-                            await bot.send(event=event, message=f"当前映射已存在 '{map_key}' - '{map_value}'")
-                            return
-                        tags_map[map_key].append(map_value)
-                        tags_quick_map[map_value] = map_key
-                        JsonUtils.update(plugin_config.filename, {
-                            "map": tags_map,
-                            "quick_map": tags_quick_map
-                        })
-                        await bot.send(event=event, message=f"映射键值对添加成功：'{map_key}' - '{map_value}'\n可以通过\n/duel map current\n查看")
-                        return
-                # 当map后面的参数为rm时，实行删除映射的操作
-                elif map_opt_params[0] in ['rm', 'remove', '删除']:
-                    if len(map_opt_params) == 1:
-                        await bot.send(event=eval, message=f"该命令在 rm 后接收两个参数，第一个参数是存在的可映射的 tag ，第二个参数是 tag 的映射。如\"duel map rm 'binary search' 二分\"")
-                        return
-                    elif len(map_opt_params) != 3:
-                        await bot.send(event=event, message=f"参数个数有误！！！\n该命令在 rm 后面只接收两个参数，请检查当前参数：{map_opt_params}\n可以使用命令\n/duel map rm\n查看命令使用详细")
-                        return
-                    map_key = map_opt_params[1]
-                    map_value = map_opt_params[2]
-                    if map_key not in tags_map:
-                        may_mention_key: list[str] = regex_search(list(tags_map.keys()), map_key)
-                        if not may_mention_key:
-                            await bot.send(event=event, message=f"输入的参数 '{map_key}' 不在标准的 tags 中，请输入\"/duel map tags\"查看可映射的tags")
-                            return
-                        else:
-                            await bot.send(event=event, message=f"输入的参数 '{map_key}' 不在标准的 tags 中\n是否在找{may_mention_key}?\n如果不是，请输入\"/duel map tags\"查看可映射的tags")
-                            return
-                    else:
-                        if map_value in tags_map[map_key]:
-                            tags_map[map_key].remove(map_value)
-                            tags_quick_map.pop(map_value)
-                            JsonUtils.update(plugin_config.filename, {
-                                "map": tags_map,
-                                "quick_map": tags_quick_map
-                            })
-                            await bot.send(event=event, message=f"映射键值对删除成功：'{map_key}' : '{map_value}'")
-                            return
-                        else:
-                            await bot.send(event=event, message=f"当前映射不存在 '{map_key}' : '{map_value}'")
-                            return
-                else:
-                    await bot.send(event=event, message=f"map 后跟了未知参数: {map_opt_params[0]}，请使用\n/duel map\n查看可用的命令")
-                    return
+        
+        command = params[0]
+        command_params = params[1:]
+        
+        command_handlers = {
+            "daily": lambda: CommandHandler.handle_daily(bot, event),
+            "day": lambda: CommandHandler.handle_daily(bot, event),
+            "problem": lambda: CommandHandler.handle_problem(bot, event, command_params),
+            "题目": lambda: CommandHandler.handle_problem(bot, event, command_params),
+            "map": lambda: CommandHandler.handle_map(bot, event, command_params),
+            "映射": lambda: CommandHandler.handle_map(bot, event, command_params)
+        }
+        
+        if command in command_handlers:
+            command_handlers[command]()
         else:
-            await bot.send(event=event, message=f"未知命令: {params[0]}")
-            return
+            await bot.send(event=event, message=f"未知命令: {command}")
+    
     except Exception as e:
         logger.opt(exception=True).error(f"[duel]响应错误: {str(e)}")
