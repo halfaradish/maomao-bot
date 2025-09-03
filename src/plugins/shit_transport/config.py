02d119e526298ccf188b7b172ac17eb3d98326ee
half <half85405@gmail.com>
2025-09-04 01:19:51 +0800

    docs: 更新搬史小助手的提示词

--- src/plugins/shit_transport/config.py
+++ src/plugins/shit_transport/config.py
@@ -15,11 +15,11 @@ class Config(BaseModel):
     api_token: str = 'Bearer ' + QQControlConfig.QQ_CONTROL_TOKEN
 
     DEFAULT_MSG: str = (
-        "[搬史] 命令使用方法\n"
-        "[别名] 搬屎 转发 banshi bs\n"
+        "[搬史/搬屎/转发/banshi/bs] 命令使用方法\n"
         "[示例]\n"
         "\"/搬史\" - 引用\回复 要搬的合并消息, 同时使用该命令, 即可将消息搬运到转发列表中的群组\n"
         "\"/搬史 list\" - 查看 '搬史小助手' 会转发哪些群组\n"
         "\"/搬史 add *group_id\" - 添加要转发的群组\n"
-        "\"/搬史 rm *group_id\" - 删除转发列表中存在的群组"
+        "\"/搬史 rm *group_id\" - 删除转发列表中存在的群组\n"
+        "\"搬史 count\" - 搬史计数器"
     )
