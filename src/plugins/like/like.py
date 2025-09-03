6fa347a8864b8b91be6243a2177d1793b8890b48
half <half85405@gmail.com>
2025-09-03 21:20:16 +0800

    feat: 新增订阅赞功能

--- src/plugins/like/like.py
+++ src/plugins/like/like.py
@@ -1,7 +1,9 @@
-from nonebot import Bot, on_command, logger, get_plugin_config, on_regex
+from nonebot import Bot, on_command, logger, get_plugin_config, on_regex, get_bot
 from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import GROUP, GroupMessageEvent, Message, MessageSegment
 from nonebot.adapters.onebot.v11.exception import ActionFailed
+from nonebot_plugin_apscheduler import scheduler
+
 import re
 
 from .config import Config
@@ -25,36 +27,80 @@ like_me = on_regex(
     priority=plugin_config.priority,
     block=plugin_config.block
 )
-
 like_other = on_regex(
     "^(超|赞)(市|)(你|他|她|它|TA|)\s*(.*)$",
     permission=GROUP,
     priority=plugin_config.priority,
     block=plugin_config.block
 )
+like_follow = on_regex(
+    r"^^(?:天天.+我|订阅赞)$$",
+    permission=GROUP,
+    priority=plugin_config.priority,
+    block=plugin_config.block
+)
+like_unfollow = on_regex(
+    r"^(?:不要?|补药|取消)\s*(?:天天.我|订阅赞)$",
+    permission=GROUP,
+    priority=plugin_config.priority,
+    block=plugin_config.block
+)
 
-def count_liked_times(user_id, count: int):
+def follow_or_not(follow: bool, user_id: str, nickname: str) -> str:
+    """改变订阅赞的用户状态"""
+    try:
+        data, _ = JsonUtils.read(filename, {
+            "liked_by_bot": {}
+        })
+        like_by_bot: dict = data.get("liked_by_bot", {})
+        user_info = like_by_bot.setdefault(str(user_id), {
+            "nickname": nickname,
+            "count": 0,
+            "follow": False
+        })
+        # 更新用户昵称（确保使用最新昵称）
+        user_info["nickname"] = nickname
+        
+        if follow != user_info.get('follow', False):
+            user_info['follow'] = follow  # 更新订阅状态
+            JsonUtils.update(filename, {"liked_by_bot": like_by_bot})  # 保存更改
+            
+            if follow:
+                return "订阅成功"
+            else:
+                return "取消订阅成功"
+        else:
+            if follow:
+                return "您已订阅，无需再次订阅"
+            else:
+                return "您未在订阅名单中，取消订阅失败"
+    except Exception as e:
+        logger.opt(exception=True).error(f"用户 {user_id}: {nickname} 订阅时发生错误: {e}")
+        return f"订阅操作失败，请稍后再试"
+
+def count_liked_times(user_id, count: int, nickname):
     """
     点赞次数计数
     """
     # 加载数据
     data, _ = JsonUtils.read(filename, {
-        "follow": {},
-        "quick_list": []
+        "liked_by_bot": {}
     })
-    follow, quick_list = data.get("follow", {}), data.get("quick_list", [])
+    liked_by_bot: dict = data.get("liked_by_bot", {})
     # 增加赞
-    if user_id not in quick_list:
-        quick_list.append(user_id)
-        follow[str(user_id)] = 0
-    follow[str(user_id)] += count
-    # 更新数据
-    JsonUtils.update(filename, {
-        "follow": follow,
-        "quick_list": quick_list
+    user = liked_by_bot.setdefault(str(user_id), {
+        "nickname": nickname,
+        "count": 0,
+        "follow": False
     })
+    user['nickname'] = nickname
+    user['count'] += count
+    JsonUtils.update(filename, {
+        "liked_by_bot": liked_by_bot
+    })
+    
 
-async def send_like(event: GroupMessageEvent, bot: Bot, user_id) -> tuple[int, any]:
+async def send_like(bot: Bot, user_id) -> tuple[int, any]:
     """
     点赞函数
 
@@ -80,13 +126,14 @@ async def send_like(event: GroupMessageEvent, bot: Bot, user_id) -> tuple[int, a
 
 @like_me.handle()
 async def like_me_handle(bot: Bot, event: GroupMessageEvent):
-    user_id = event.user_id
+    user_id = event.sender.user_id
+    nickname = event.sender.nickname
 
     if user_id:
         sender: Message = Message([MessageSegment.at(user_id=event.user_id)])
         count, err_msg = await send_like(event=event, bot=bot, user_id=user_id)
         if count > 0:
-            count_liked_times(user_id=user_id, count=count)
+            count_liked_times(user_id=user_id, count=count, nickname=nickname)
             await like_me.finish("已经给 " + sender + f" 点赞 {count} 次\n点赞的送达可能会有延迟, 如果失败了可以添加好友再试")
         else:
             if err_msg and isinstance(err_msg, dict) and err_msg.get("message") is not None and err_msg.get("message") != "":
@@ -108,6 +155,8 @@ async def like_other_handle(bot: Bot, event: GroupMessageEvent):
         return
 
     user_id = int(match_result.group(0))
+    user_info = await bot.get_stranger_info(user_id=int(user_id))
+    nickname = user_info["nickname"]
 
     if user_id:
         try:
@@ -119,7 +168,7 @@ async def like_other_handle(bot: Bot, event: GroupMessageEvent):
 
         count, err_msg = await send_like(event=event, bot=bot, user_id=user_id)
         if count > 0:
-            count_liked_times(user_id=user_id, count=count)
+            count_liked_times(user_id=user_id, count=count, nickname=nickname)
             await like_other.finish("成功帮" + sender + " 给 " + likeder + f" 点赞 {count} 次")
         else:
             if err_msg and isinstance(err_msg, dict) and err_msg.get("message") is not None and err_msg.get("message") != "":
@@ -128,3 +177,51 @@ async def like_other_handle(bot: Bot, event: GroupMessageEvent):
                 await like_other.finish(sender + " , 无法给指定的人 " + likeder + " 更多赞了哦")
     else:
         await like_other.finish(sender + "未指定有效的QQ号或@用户")
+
+@like_follow.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    """添加订阅"""
+    follow: bool = True
+    user_id = event.sender.user_id
+    nickname = event.sender.nickname
+    msg = follow_or_not(follow=follow, user_id=user_id, nickname=nickname)
+    await like_unfollow.finish(msg)
+
+@like_unfollow.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    """取消订阅"""
+    follow: bool = False
+    user_id = event.sender.user_id
+    nickname = event.sender.nickname
+    msg = follow_or_not(follow=follow, user_id=user_id, nickname=nickname)
+    await like_unfollow.finish(msg)
+
+@scheduler.scheduled_job("cron", hour=6, minute=0 ,second=0, id="job_subscribed_likes")
+async def _():
+    "给订阅的用户进行每日点赞"
+    # 获取数据
+    bot = get_bot()
+    data, _ = JsonUtils.read(filename, {
+        "liked_by_bot": {}
+    })
+    liked_by_bot: dict = data.get("liked_by_bot", {})
+    # 为订阅的用户点赞
+    for user_id, user_info in liked_by_bot.items():
+        if user_info['follow']:
+            try:
+                count, err_msg = await send_like(bot=bot, user_id=user_id)
+                if count > 0:
+                    user_info = await bot.get_stranger_info(user_id=int(user_id))
+                    nickname = user_info["nickname"]
+                    count_liked_times(user_id=user_id, count=count, nickname=nickname)
+                    liked_by_bot[user_id]['nickname'] = nickname
+                    liked_by_bot[user_id]['count'] += count
+                else:
+                    logger.error(err_msg)
+            except Exception as e:
+                logger.error(f"Error occurred while sending like to user {user_id}: {e}")
+                continue
+    # 存入数据
+    JsonUtils.update(filename, {
+        "liked_by_bot": liked_by_bot
+    })
