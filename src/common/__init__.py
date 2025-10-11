46db31d9707645d2923c4d84d8f2262a9b478dcc
half <half85405@gmail.com>
2025-10-12 00:26:11 +0800

    feat: [common]隐藏了发送合并转发消息的细节，可使用字符串列表直接发送合并转发消息

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -2,6 +2,13 @@ from . import icpc_db_pool
 from .icpc_db_pool import get_icpc_db_connection
 from .json_utils import JsonUtils
 from .compress_pics import CompressPic
-import utils
+from .send_forward_msg import SendForwardMsg
+from . import utils
 
-__all_ = ['get_icpc_db_connection', 'JsonUtils', 'CompressPic', 'utils']
+__all__ = [
+    'get_icpc_db_connection', 
+    'JsonUtils', 
+    'CompressPic', 
+    'SendForwardMsg',
+    'utils'
+]
