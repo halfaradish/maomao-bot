18075b088f660713c88f9aebfa7a7475b4446775
half <half85405@gmail.com>
2025-10-23 13:51:49 +0800

    feat[leaderboard]: 过题积分榜添加cf积分

--- src/plugins/leaderboard/config.py
+++ src/plugins/leaderboard/config.py
@@ -8,7 +8,7 @@ class Config(BaseModel):
         "[参数]\n"
         "现役、退役、预备役"
     )
-    # https://acm.gxu.edu.cn/gxuicpc/luogu/scores
-    qingluan_scores_data_base_url: str = "http://127.0.0.1:8090/luogu/scores"
+    # https://acm.gxu.edu.cn/gxuicpc/scores/all
+    qingluan_scores_data_base_url: str = "http://127.0.0.1:8090/scores/all"
 
     timeout: float = 3.0
