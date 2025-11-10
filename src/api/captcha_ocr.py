197eed321fa01f27ad2732bfe279200a8afa9858
half <half85405@gmail.com>
2025-11-11 02:00:57 +0800

    fix[api/captcha_ocr]: 修复了 ocr api 的 token 只能以query参数传输的问题

--- src/api/captcha_ocr.py
+++ src/api/captcha_ocr.py
@@ -1,5 +1,12 @@
 from nonebot import logger
-from fastapi import APIRouter, Request, Query, HTTPException, UploadFile, File
+from fastapi import (
+    APIRouter,
+    Request,
+    Query,    
+    UploadFile,
+    File,
+    Form
+)
 from pydantic import BaseModel
 import io
 from functools import wraps
@@ -13,9 +20,6 @@ router = APIRouter()
 class UrlItem(BaseModel):
     token: str | None = None
     url: str
-
-class BytesItem(BaseModel):
-    token: str | None = None
 
 @router.get('/hello')
 async def hello(request: Request):
@@ -63,8 +67,9 @@ async def recognize_from_url_server(
 @router.post('/bytes')
 async def recognize_from_bytes_server(
     *, 
-    token: str = Query(''),
+    query_token: str = Query(''),
     file: UploadFile = File(...),
+    token: str | None = Form(...),
     req: Request
 ):
     """
@@ -73,8 +78,9 @@ async def recognize_from_bytes_server(
     """
     try:
         # 检查是否传入token
-        if not token:
+        if not query_token and not token:
             return error(message="token未传入", request=req)
+        token = token if token else query_token
 
         # 检查token是否正确
         if not token == NoneBotToken.DITING_API_ACCESS_TOKEN:
@@ -88,51 +94,6 @@ async def recognize_from_bytes_server(
         
         # ocr识别
         captcha: str = recognize(image=image_bytes)
-        logger.debug(f"成功识别验证码：{captcha}")
-
-        return success(
-            data={
-                "captcha": captcha
-            },
-            request=req
-        )
-    except Exception as e:
-        logger.error(f"{req.url.path} 发生错误: {e}")
-        return error(
-            message=str(e),
-            request=req
-        )
-
-
-@router.post('/bytesio')
-async def recognize_from_bytesio_server(
-    *, 
-    token: str = Query(''),
-    file: UploadFile = File(...),
-    req: Request
-):
-    """
-    通过BytesIO对象识别验证码
-    使用文件上传的方式，后端转换为BytesIO对象
-    """
-    try:
-        # 检查是否传入token
-        if not token:
-            return error(message="token未传入", request=req)
-
-        # 检查token是否正确
-        if not token == NoneBotToken.DITING_API_ACCESS_TOKEN:
-            return error(
-                code=403,
-                request=req
-            )
-        
-        # 读取文件内容并转换为BytesIO对象
-        image_bytes = await file.read()
-        bytesio_object = io.BytesIO(image_bytes)
-        
-        # ocr识别
-        captcha: str = recognize(image=bytesio_object)
         logger.debug(f"成功识别验证码：{captcha}")
 
         return success(
