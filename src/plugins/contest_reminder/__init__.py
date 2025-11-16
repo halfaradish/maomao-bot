6f362f5b6170f550fbe3d39197556655a79d501f
half <half85405@gmail.com>
2025-11-16 13:54:43 +0800

    feat[contest_reminder]: 添加了比赛前提醒的功能

--- src/plugins/contest_reminder/__init__.py
+++ src/plugins/contest_reminder/__init__.py
@@ -2,7 +2,8 @@ from nonebot import (
     get_plugin_config,
     logger,
     on_command,
-    require
+    require,
+    get_bot
 )
 from nonebot.adapters.onebot.v11 import (
     Bot,
@@ -14,13 +15,16 @@ from nonebot.plugin import PluginMetadata
 from nonebot.params import CommandArg
 from nonebot.exception import FinishedException
 require("nonebot_plugin_apscheduler")
-import nonebot_plugin_apscheduler
+from nonebot_plugin_apscheduler import scheduler
 
-from typing import List
+from typing import List, Dict
+from uuid import uuid4
+from datetime import datetime, timedelta
 
 from .config import Config
 from .contest_fetcher import contest_fetcher, ContestInfo
 from ...common.send_forward_msg import send_forword_msg
+from ...common import JsonUtils
 
 __plugin_meta__ = PluginMetadata(
     name="contest_reminder",
@@ -43,7 +47,7 @@ get_contest_info = on_command(
 )
 
 @get_contest_info.handle()
-async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()) -> None:
     # 获取天数
     raw_args: str = args.extract_plain_text().strip()
     # 获取偏移时间
@@ -79,3 +83,85 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
         pass
     except Exception as e:
         logger.error(f"获取比赛信息出错: {e}")
+
+def get_json_data() -> Dict:
+    """读取json配置文件"""
+    try:
+        data, _ = JsonUtils.read(
+            filename=config.clist_filename,
+            default={
+                "groups_send_by_plugin": []
+            }
+        )
+        return data
+    except Exception as e:
+        logger.error(f"读取json文件时出错: {type(e).__name__}: {e}")
+        # 返回默认值，确保程序可以继续运行
+        return {
+            "groups_send_by_plugin": []
+        }
+
+async def remind_contest_to_groups(contest: ContestInfo) -> None:
+    """发送比赛提醒消息"""
+    try:
+        bot: Bot = get_bot()
+        # 获取数据
+        data: Dict = get_json_data()
+        group_ids: List[str] = data.get('groups_send_by_plugin', [])
+        
+        if not group_ids:
+            logger.debug("没有需要发送提醒的群组")
+            return
+
+        # 发送消息
+        for group_id in group_ids:
+            try:
+                await bot.send_group_msg(
+                    group_id=group_id,
+                    message= (
+                        "比赛提醒：\n"
+                        f"{contest.to_string()}"
+                    )
+                )
+                logger.debug(f"成功向群组 {group_id} 发送比赛提醒")
+            except Exception as e:
+                logger.error(f"向群组 {group_id} 发送消息时出错: {e}")
+                # 继续处理下一个群组，不中断整体流程
+                continue
+    except Exception as e:
+        logger.error(f"发送消息时出错: {e}")
+        return
+
+
+@scheduler.scheduled_job(
+    "cron",
+    hour=config.clist_remind_run_time_hour,
+    minute=0,
+    second=0,
+    id="contests_reminder"
+)
+async def contest_reminder():
+    """每天固定时间赛程提醒"""
+    # 获取比赛信息
+    contests: List[ContestInfo] = contest_fetcher.fetch_contests(platform_names=config.clist_platforms)
+
+    success_cnt = 0
+    for contest in contests:
+        run_date: datetime = contest.start - timedelta(hours=1)
+        # 尝试安排提醒
+        try:
+            job_id=f"contest_reminder_{uuid4().hex[:8]}"
+            scheduler.add_job(
+                remind_contest_to_groups,
+                'date',
+                run_date=run_date,
+                args=[contest],
+                id=job_id
+            )
+            success_cnt += 1
+
+        except Exception as e:
+            logger.error(f"安排比赛提醒时出错: {e}")
+            continue
+
+    logger.info(f"成功安排 {success_cnt} 条比赛提醒")
