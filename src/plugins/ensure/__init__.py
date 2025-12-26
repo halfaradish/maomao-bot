0c1b4642461e07157bb410a4fa587e768c3c3556
half <half85405@gmail.com>
2025-12-26 20:37:26 +0800

    feat[logger]: 日志可持久化

--- src/plugins/ensure/__init__.py
+++ src/plugins/ensure/__init__.py
@@ -4,3 +4,15 @@ nonebot项目启动时会立刻加载src/plugins内的插件
 即设置ensure插件导入需要在启动时加载的服务
 """
 from ...api import app
+from nonebot.log import logger, default_format
+
+logger.add(
+    'logs/nonebot.log',
+    rotation="00:00",
+    retention="7 days",
+    level="DEBUG",
+    encoding="utf-8",
+    format=default_format,
+    enqueue=True,
+    compression="zip"
+)
