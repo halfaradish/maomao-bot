97b870141ed6b99a647469834702601a4f74ccde
xqcherry <h2417185282@outlook.com>
2026-03-08 09:45:51 +0800

    fix[llm_scribe]: 统一为相对路径导入

---
+++ src/plugins/llm_scribe/__init__.py
@@ -0,0 +1,10 @@
+from nonebot.plugin import PluginMetadata
+from .src import __all__
+
+__plugin_meta__ = PluginMetadata(
+    name="llm_scribe",
+    description="",
+    usage="",
+    config="",
+    extra={}
+)
