b78dc0045bb2138ec7b914997946e92472a20264
half <half85405@gmail.com>
2026-03-19 20:13:01 +0800

    fix(auto_manage_group): 添加插件开关

--- src/plugins/auto_manage_group/config.py
+++ src/plugins/auto_manage_group/config.py
@@ -7,3 +7,7 @@ class Config(BaseModel):
     context_erase_time_range: int = 30 # 上下文时间范围（秒），默认5分钟
     context_erase_delay: int = 5  # 延迟执行时间（秒），默认5秒
     context_erase_retry_delay: float = 0.1  # 撤回间隔延迟（秒），默认0.5秒
+
+    auto_manage_banned_word_enable: bool = False
+    auto_manage_increase: bool = False
+    auto_manage_decrease: bool = False
