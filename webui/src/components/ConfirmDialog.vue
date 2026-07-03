4dd9823d033f88991c8eb4678801bb8c7cdb31d1
half <half85405@gmail.com>
2026-07-03 14:54:13 +0800

    chore(perm): 将二次确认框的‘确认’和‘取消’键对换

--- webui/src/components/ConfirmDialog.vue
+++ webui/src/components/ConfirmDialog.vue
@@ -4,8 +4,8 @@
       <h3><WarningCircle :size="20" />{{ confirmState.title }}</h3>
       <p>{{ confirmState.message }}</p>
       <div class="confirm-actions">
-        <button class="secondary" @click="confirmState.reject"><X :size="14" />取消</button>
         <button class="contrast" @click="confirmState.resolve"><Check :size="14" />确认</button>
+        <button class="secondary" @click="confirmState.reject"><X :size="14" />取消</button>
       </div>
     </div>
   </div>
